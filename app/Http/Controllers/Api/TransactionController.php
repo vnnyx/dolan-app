2be53a6940ad27ca1bbe3be2d550923ad0b1d01b
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-06-08 16:21:48 +0700

    feat: transaction api

---
+++ app/Http/Controllers/Api/TransactionController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Helper\WebResponse;
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use App\Models\Wisata;
+use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+
+class TransactionController extends Controller
+{
+    public function listHistoryTicket()
+    {
+        $result = Transaction::query()
+            ->join('wisatas', 'transactions.nama_wisata', '=', 'wisatas.nama_wisata')
+            ->select(['transactions.*', 'wisatas.alamat'])
+            ->where('transactions.username', '=', auth('api')->payload()->get('username'))
+            ->orderBy('created_at', 'desc')
+            ->get();
+        return $this->tiketResponse($result);
+    }
+
+    public function order(Request $request)
+    {
+        $field = $request->validate([
+            'total_amount' => 'required',
+            'destination_id' => 'required',
+        ]);
+        $path = $request->proof_of_payment->storeOnCloudinary('abp')->getSecurePath();
+        $nama_wisata = Wisata::query()->select('nama_wisata')->where('id', '=', $field['destination_id'])->value('nama_wisata');
+        Transaction::create([
+            'username' => auth('api')->payload()->get('username'),
+            'nama_wisata' => $nama_wisata,
+            'total_ticket' => $field['total_amount'],
+            'bukti_pembayaran' => $path
+        ]);
+
+        return WebResponse::webResponse(200, "OK");
+    }
+
+    private function tiketResponse(Collection $result): JsonResponse
+    {
+        Carbon::setLocale('id');
+        $data = [];
+        for ($i = 0; $i < sizeof($result); $i++) {
+            $status = match ($result[$i]['status']) {
+                0 => "Menunggu",
+                1 => "Aktif",
+                default => "Kadaluarsa",
+            };
+            $data[] = [
+                'ticket_id' => $result[$i]['id'],
+                'destination_name' => $result[$i]['nama_wisata'],
+                'location' => $result[$i]['alamat'],
+                'date' => $result[$i]['created_at']->isoFormat('dddd, D MMMM Y'),
+                'status' => $status,
+                'barcode' => Str::random(),
+            ];
+        }
+        return WebResponse::webResponse(200, 'OK', $data);
+    }
+}
