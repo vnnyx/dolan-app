db6bcc8101b14e10e55470bb03f4d254476e86f9
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-06-09 13:15:22 +0700

    feat: forgot password

---
+++ resources/views/emails/send-otp.blade.php
@@ -0,0 +1,14 @@
+@component('mail::message')
+    # Halo {{$username}}
+
+    Kami menerima permintaan reset password dari anda. Silahkan gunakan kode dibawah ini untuk melakukan reset password
+
+    {{$otp}}
+
+
+    Thanks,
+
+    Tim Dolan
+@endcomponent
+
+
