821cdc4cc0f1bb906d779155680e6f9162064205
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-06-13 21:46:00 +0700

    add new seeder

--- database/seeders/ContentSeeder.php
+++ database/seeders/ContentSeeder.php
@@ -38,6 +38,69 @@ class ContentSeeder extends Seeder
                 'created_at'=>Carbon::now(),
                 'updated_at'=>Carbon::now()
             ],
+            [
+                'username'=>'Muhammad Aryuska',
+                'nama_wisata'=>'Lembang Zoo Park',
+                'content'=>'https://res.cloudinary.com/dxbmfujzo/image/upload/v1655130575/abp/5e2d3bc107798_zww2bh.jpg',
+                'created_at'=>Carbon::now(),
+                'updated_at'=>Carbon::now()
+            ],
+            [
+                'username'=>'Muhammad Aryuska',
+                'nama_wisata'=>'Lembang Zoo Park',
+                'content'=>'https://res.cloudinary.com/dxbmfujzo/image/upload/v1655130575/abp/Lembang-Park-Zoo-Bandung_dtgduo.jpg',
+                'created_at'=>Carbon::now(),
+                'updated_at'=>Carbon::now()
+            ],
+            [
+                'username'=>'Muhammad Aryuska',
+                'nama_wisata'=>'Lembang Zoo Park',
+                'content'=>'https://res.cloudinary.com/dxbmfujzo/image/upload/v1655130575/abp/429818817_iirru5.jpg',
+                'created_at'=>Carbon::now(),
+                'updated_at'=>Carbon::now()
+            ],
+            [
+                'username'=>'Firdaus Putra',
+                'nama_wisata'=>'Jurug',
+                'content'=>'https://res.cloudinary.com/dxbmfujzo/image/upload/v1655130728/abp/sedang_1502986552ju_vqbrvt.jpg',
+                'created_at'=>Carbon::now(),
+                'updated_at'=>Carbon::now()
+            ],
+            [
+                'username'=>'Firdaus Putra',
+                'nama_wisata'=>'Jurug',
+                'content'=>'https://res.cloudinary.com/dxbmfujzo/image/upload/v1655130728/abp/jurug-waru-trenggalek-jawa-timur_lt5xex.jpg',
+                'created_at'=>Carbon::now(),
+                'updated_at'=>Carbon::now()
+            ],
+            [
+                'username'=>'Firdaus Putra',
+                'nama_wisata'=>'Jurug',
+                'content'=>'https://res.cloudinary.com/dxbmfujzo/image/upload/v1655130727/abp/20c69588-5735-4d55-8e2e-a4fd19bbc366_169_gsrpjg.jpg',
+                'created_at'=>Carbon::now(),
+                'updated_at'=>Carbon::now()
+            ],
+            [
+                'username'=>'Ignasius Nindra',
+                'nama_wisata'=>'Lawang Sewu',
+                'content'=>'https://res.cloudinary.com/dxbmfujzo/image/upload/v1655130843/abp/cover_13_iaw9ys.jpg',
+                'created_at'=>Carbon::now(),
+                'updated_at'=>Carbon::now()
+            ],
+            [
+                'username'=>'Ignasius Nindra',
+                'nama_wisata'=>'Lawang Sewu',
+                'content'=>'https://res.cloudinary.com/dxbmfujzo/image/upload/v1655130843/abp/5f06e3f4c5898jpg-20210914021347_beh5iy.jpg',
+                'created_at'=>Carbon::now(),
+                'updated_at'=>Carbon::now()
+            ],
+            [
+                'username'=>'Ignasius Nindra',
+                'nama_wisata'=>'Lawang Sewu',
+                'content'=>'https://res.cloudinary.com/dxbmfujzo/image/upload/v1655130843/abp/5ea13b83807c8_yeqqgr.jpg',
+                'created_at'=>Carbon::now(),
+                'updated_at'=>Carbon::now()
+            ],
         ]);
     }
 }
