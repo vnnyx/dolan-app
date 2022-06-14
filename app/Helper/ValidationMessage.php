658b697c6fc21c5beba86140af8a2e47da4d18c0
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-06-14 13:01:48 +0700

    add validation message

---
+++ app/Helper/ValidationMessage.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Helper;
+
+class ValidationMessage
+{
+    public static function message(){
+        return [
+            'username.required'=>'Username harus diisi',
+            'username.unique'=>'Username sudah digunakan',
+            'email.required'=>'Email harus diisi',
+            'password.min'=>'Panjang password minimal 8',
+            'password.required'=>'Password harus diisi',
+            'password.confirmed'=>'Password tidak cocok',
+            'total_amount.required'=>'Jumlah tiket harus diisi',
+            'destination_id.required'=>'Tempat wisata harus diisi',
+        ];
+    }
+}
